ad7695e05dc0c84954accc537c9da80d326db579
kenanhancer <kenanhancer@gmail.com>
2020-04-15 21:56:28 +0100

    first commit

---
+++ src/main/java/com/kenanhancer/helloworld/maven/Application.java
@@ -0,0 +1,11 @@
+package com.kenanhancer.helloworld.maven;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Greeter greeter = new Greeter();
+
+        System.out.println(greeter.sayHello());
+    }
+}
---
+++ src/main/java/com/kenanhancer/helloworld/maven/Greeter.java
@@ -0,0 +1,7 @@
+package com.kenanhancer.helloworld.maven;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
